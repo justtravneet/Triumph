a226d2a274e7852da16a21f3de869d74f16880cb
justtravneet <singhji6868@gmailcom>
2024-06-25 04:26:22 -0700

    responsivenav

--- src/components/Herooutdoor.jsx
+++ src/components/Herooutdoor.jsx
@@ -5,11 +5,11 @@ function Herooutdoor() {
  return (
   <div className='px-[10px] py-[5px] sm:px-[25px] sm:py-[5px] md:px-[35px] lg:px-[65px] xl:px-[100px] 2xl:justify-center 2xl:gap-[100px] '>
           <div className=' flex justify-center gap-[10px]  flex-wrap'>
-               <div className='w-[170px] sm:w-[180px] h-[220px] bg-slate-300 px-[5px] py-[5px] outline'>
+               <div className='w-[140px] sm:w-[180px] h-[220px] bg-slate-300 px-[5px] py-[5px] outline'>
                     <div className='w-[100%] h-[120px] bg-slate-600 '></div>
                     <div className='w-[100%] h-[90px] bg-slate-800 '></div>
                </div>
-               <div className='w-[170px] h-[220px] bg-slate-300 px-[5px] py-[5px] outline'>
+               <div className='w-[140px] h-[220px] bg-slate-300 px-[5px] py-[5px] outline'>
                     <div className='w-[100%] h-[120px] bg-slate-600 '></div>
                     <div className='w-[100%] h-[90px] bg-slate-800 '></div>
                </div>
