2dc685dcb455ef33ded62d1a968be0d80d494537
justtravneet <singhji6868@gmailcom>
2024-07-05 08:18:28 -0700

    responsivenav

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import { Select, Space } from 'antd';
 import { Input } from 'antd';
+import { Button } from 'antd';
 import { ChevronDown } from 'lucide-react';
 const Search = () => {
 
@@ -23,6 +24,7 @@ const Search = () => {
             image: "https://i.pinimg.com/originals/9e/e0/ed/9ee0ed3fd1380ccd7ded429b56c1d8b3.jpg",
             price: "Rs.000/-",
         },
+
         {
             name: "Boat Heaphones",
             title: "Rounded surface with SOFT CUSHIONS",
@@ -47,81 +49,145 @@ const Search = () => {
             image: "https://i.pinimg.com/originals/9e/e0/ed/9ee0ed3fd1380ccd7ded429b56c1d8b3.jpg",
             price: "Rs.000/-",
         },
-        
         {
             name: "Boat Heaphones",
             title: "Rounded surface with SOFT CUSHIONS",
             image: "https://i.pinimg.com/originals/9e/e0/ed/9ee0ed3fd1380ccd7ded429b56c1d8b3.jpg",
             price: "Rs.000/-",
         },
-        {
-            name: "Boat Heaphones",
-            title: "Rounded surface with SOFT CUSHIONS",
-            image: "https://i.pinimg.com/originals/9e/e0/ed/9ee0ed3fd1380ccd7ded429b56c1d8b3.jpg",
-            price: "Rs.000/-",
-        },
-        {
-            name: "Boat Heaphones",
-            title: "Rounded surface with SOFT CUSHIONS",
-            image: "https://i.pinimg.com/originals/9e/e0/ed/9ee0ed3fd1380ccd7ded429b56c1d8b3.jpg",
-            price: "Rs.000/-",
-        },
-        
+      
+
 
     ]
 
 
     return (
         <div className='  px-[10px] py-[5px] sm:px-[25px] sm:py-[5px] md:px-[35px] lg:px-[65px] xl:px-[100px] xl:justify-center flex 2xl:justify-center 2xl:gap-[100px]'>
-           <div className='responsivesearch  w-[700px] md:w-[1000px] lg:w-[1200px] h-[500px]  bg-slate-100'>
-                  
-                  <div className='filtersres w-[100%] h-[40px]   bg-slate-400'>
-                        <div className='px-[2px] flex   justify-around w-[50%] '>
-                            <div className=' filterbutton flex  items-center gap-[3px] px-[6px] border'>
+            <div className='responsivesearch  w-[700px] md:w-[1000px] lg:w-[1200px] h-[auto]  bg-slate-100'>
+
+                <div className='filtersres w-[100%] h-[40px]   bg-slate-400'>
+                    <div className='px-[2px] flex   justify-around w-[50%] '>
+
+                        <div>
+                            <Select
+                                defaultValue="lucy"
+                                style={{ width: 80 }}
+
+                                options={[
+                                    { value: 'jack', label: 'lights' },
+                                    { value: 'lucy', label: 'Lucy' },
+                                    { value: 'Yiminghe', label: 'yiminghe' },
+                                    { value: 'disabled', label: 'Disabled', disabled: true },
+                                ]}
+                            />
+                        </div>
+                        <div>
+                            <Select
+                                defaultValue="lucy"
+                                style={{ width: 80 }}
+
+                                options={[
+                                    { value: 'jack', label: 'Color' },
+                                    { value: 'lucy', label: 'Lucy' },
+                                    { value: 'Yiminghe', label: 'yiminghe' },
+                                    { value: 'disabled', label: 'Disabled', disabled: true },
+                                ]}
+                            />
+                        </div>
+                        {/* <div className=' filterbutton flex  items-center gap-[3px] px-[6px]  '>
                                  <div><p className='text-[13px]'>Color</p> </div>
                                  <div className='w-[16px] flex items-center'> <ChevronDown /></div>
-                            </div>
-                            
+                            </div> */}
+                    </div>
 
-                            <div className=' filterbutton flex  items-center gap-[3px] px-[6px]  '>
-                                 <div><p className='text-[13px]'>Color</p> </div>
-                                 <div className='w-[16px] flex items-center'> <ChevronDown /></div>
-                            </div>
-                        </div>
-
-                        <div className=' w-[50%] px-[4px]'>
-                           <Input placeholder="Search here" />
-                        </div>
-                  </div>
-
-                  <div className='drop'>
-                     
-                  </div>
-
-                 
-           </div>
-
-           
-
-          
-
-
-            <div className='desktopsearch w-[700px] md:w-[1000px] lg:w-[1200px] h-[500px] flex bg-slate-400'>
-
-                <div className='w-[30%] sm:w-[20%] md:w-[20%] h-[500px] bg-slate-500 px-[8px] py-[5px] flex flex-col gap-[10px]'>
-                            <div className=' filterbutton flex   justify-between gap-[3px] px-[8px]  '>
-                                 <div><p className='text-[14px]'>Color</p> </div>
-                                 <div className='w-[18px] flex items-center'> <ChevronDown /></div>
-                            </div>
-
-                            <div className=' filterbutton flex   justify-between gap-[3px] px-[8px]  '>
-                                 <div><p className='text-[14px]'>Color</p> </div>
-                                 <div className='w-[18px] flex items-center'> <ChevronDown /></div>
-                            </div>
-                     
+                    <div className=' w-[50%] px-[4px]'>
+                        <Input placeholder="Search here" />
+                    </div>
                 </div>
 
-                <div className=' w-[100%] bg-slate-200 h-[500px]'>
+
+                <div className='h-[auto] w-[100%] flex justify-center  gap-[25px]  flex-wrap   p-3'>
+                    {data.map((d) => (
+
+                        <div className='cardconatiner-back  w-[150px] h-[auto] '>
+                            <div className='card-image-back w-[150px] h-[50px]'>
+                                <div className='card-images w-[150px] h-[50px]'>
+                                    <img className='cart-images' src={d.image} alt="" />
+                                </div>
+                            </div>
+                            <div className='info-back w-[150px] h-[90px] mt-[110px]   flex justify-between flex-col '>
+                                <div className='information w-[150px]  h-[90px] flex flex-col p-2'>
+                                    <p className='text-[13px] font-medium'>{d.name}</p>
+                                    <p className='text-[8px] font-light'>{d.title}</p>
+                                    <div className='flex gap-[5px] items-center'>
+                                        <div><p className='text-[11px] font-normal'>Color</p></div>
+                                        <div><p className=' bg-gray-700 h-[10px] w-[10px] rounded-xl'></p></div>
+                                        <div><p className=' bg-gray-100 h-[10px] w-[10px] rounded-xl'></p></div>
+                                        <div><p className=' bg-gray-200 h-[10px] w-[10px] rounded-xl'></p></div>
+                                    </div>
+                                    <p className='text-[12px] font-semibold'>{d.price}</p>
+                                </div>
+
+
+                            </div>
+                            <div className='h-[30px] w-[150px] '>
+                                <Button className='button w-[100%] h-[100%] rounded-none' type="primary"><p className='text-[13px]'>Add To Cart</p></Button>
+                            </div>
+
+
+
+                        </div>
+
+
+
+                    ))}
+
+                </div>
+
+
+
+
+            </div>
+
+
+
+
+
+
+            <div className='desktopsearch w-[700px] md:w-[1000px] lg:w-[1200px] h-[auto] flex bg-slate-400'>
+
+                <div className='w-[30%] sm:w-[20%] md:w-[20%] h-[auto] bg-slate-500 px-[8px] py-[5px] flex flex-col gap-[10px]'>
+                    <div>
+                        <Select
+                            defaultValue="Color"
+                            style={{ width: 120 }}
+
+                            options={[
+                                { value: 'jack', label: 'Jack' },
+                                { value: 'lucy', label: 'Lucy' },
+                                { value: 'Yiminghe', label: 'yiminghe' },
+                                { value: 'disabled', label: 'Disabled', disabled: true },
+                            ]}
+                        />
+                    </div>
+
+                    <div>
+                        <Select
+                            defaultValue="Lights"
+                            style={{ width: 120 }}
+
+                            options={[
+                                { value: 'jack', label: 'Jack' },
+                                { value: 'lucy', label: 'Lucy' },
+                                { value: 'Yiminghe', label: 'yiminghe' },
+                                { value: 'disabled', label: 'Disabled', disabled: true },
+                            ]}
+                        />
+                    </div>
+
+                </div>
+
+                <div className=' w-[100%] bg-slate-200 h-[auto]'>
                     <div className='px-[10px] py-[5px]'>
                         <Input placeholder="Search For Lights" variant="filled" />
                     </div>
@@ -129,15 +195,42 @@ const Search = () => {
 
                     <div>
 
-                    <div className='w-[100%] h-[100%] flex flex-wrap gap-[5px] px-[10px] py-[5px] '>
-                             {data.map((d) => (
-                                   <div className='w-[auto] h-[150px] bg-slate-400'>
+                        <div className=' justify-center w-[100%] h-[100%] flex  flex-wrap gap-[25px] px-[10px] py-[5px] '>
+                            {data.map((d) => (
+
+                                <div className='cardconatiner-back  w-[150px] h-[290px] '>
+                                    <div className='card-image-back w-[150px] h-[50px]'>
+                                        <div className='card-images w-[150px] h-[50px]'>
+                                            <img className='cart-images' src={d.image} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className='info-back w-[150px] h-[100px] mt-[110px]   flex justify-between flex-col '>
+                                        <div className='information w-[150px]  h-[100px] flex flex-col p-2'>
+                                            <p className='text-[14px] font-medium'>{d.name}</p>
+                                            <p className='text-[9px] font-light'>{d.title}</p>
+                                            <div className='flex gap-[5px] items-center'>
+                                                <div><p className='text-[12px] font-normal'>Color</p></div>
+                                                <div><p className=' bg-gray-700 h-[10px] w-[10px] rounded-xl'></p></div>
+                                                <div><p className=' bg-gray-100 h-[10px] w-[10px] rounded-xl'></p></div>
+                                                <div><p className=' bg-gray-200 h-[10px] w-[10px] rounded-xl'></p></div>
+                                            </div>
+                                            <p className='text-[14px] font-semibold'>{d.price}</p>
+                                        </div>
+
 
                                     </div>
-                                    
+                                    <div className='h-[30px] w-[150px] '>
+                                        <Button className='button w-[100%] h-[100%] rounded-none' type="primary"><p className='text-[13px]'>Add To Cart</p></Button>
+                                    </div>
 
-                             ))}
-                         </div>
+
+
+                                </div>
+
+
+
+                            ))}
+                        </div>
 
                     </div>
                 </div>
